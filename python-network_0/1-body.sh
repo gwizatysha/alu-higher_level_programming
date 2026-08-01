79a4f738409d5d52a87371b4b8a0b4d5a7a329da
gwizatysha <g.kansiime@alustudent.com>
2026-08-01 01:50:29 -0700

    Fix 1-body.sh to follow redirects

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# displays the body of the response only if status code is 200
-curl -s -f "$1"
+# displays the body of the response only if final status code is 200
+curl -s -L -f "$1"
